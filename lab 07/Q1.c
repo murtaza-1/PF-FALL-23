a2250ce7adbc8d79059b83041312454d7acef8b4
murtaza-1 <142867464+murtaza-1@users.noreply.github.com>
2023-10-10 09:30:08 +0500

    Update Q1.c

--- lab 07/Q1.c
+++ lab 07/Q1.c
@@ -1,1 +1,18 @@
-
+#include<stdio.h>
+ 
+ main()
+{
+	int a,x,y;
+	printf("enter number 1:");
+	scanf("%d",&x);
+	printf("enter number 2:");
+	scanf("%d",&y);
+    a=1;
+  if(x==0 || y==0)
+  printf("\nerror");
+   while(x>y)
+   {
+   	x=x-y;
+   	a++;
+   }
+  }
