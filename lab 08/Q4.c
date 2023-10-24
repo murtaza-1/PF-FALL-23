222a347f38ad2671c2039597b7da911553a16be0
murtaza-1 <142867464+murtaza-1@users.noreply.github.com>
2023-10-24 10:27:54 +0500

    Update Q4.c

--- lab 08/Q4.c
+++ lab 08/Q4.c
@@ -1,28 +1,26 @@
-Q4
+
 
 #include <stdio.h>
 #include <string.h>
- 
-int main(void)
-{
-  char a[100] ;
-  char * ptr;
-  int ch;
-  int count;
-  printf("enter string");
-  gets(a);
-  printf("enter letter to search");
-  scanf("%c",&ch);
- 
-  ptr = strchr( a, ch );
-  printf( "The first occurrence of %c in '%s' is '%s'\n",
-            ch, a, ptr );
- 
- while(ptr!=NULL)
- {
- 	count++;
- 	ptr=strchr(ptr+1,ch);
- }
- printf("%d",count);
+
+int main() {
+    char text[100];
+    char searchChar;
+    int count = 0,i;
+
+    printf("Enter a string: ");
+    fgets(text, sizeof(text), stdin);
+
+    printf("Enter a character to search for: ");
+    scanf(" %c", &searchChar); // Note the space before %c to consume any leading whitespace.
+
+    for (i = 0; text[i] != '\0'; i++) {
+        if (text[i] == searchChar) {
+            count++;
+        }
+    }
+
+    printf("Total occurrences of '%c' in the text: %d\n", searchChar, count);
+
+    return 0;
 }
- 
