b08139e5ee09aa17cfb7ddaed0511fe60e7e8736
murtaza-1 <142867464+murtaza-1@users.noreply.github.com>
2023-10-31 09:56:04 +0500

    Update Q3.c

--- lab 09a/Q3.c
+++ lab 09a/Q3.c
@@ -1,36 +1,44 @@
-
 #include<stdio.h>
 #include<stdlib.h>
 
 main() {
 int n;
-printf("enter the size of the arrays");
+printf("enter the size of all the arrays");
 scanf("%d",&n);
 
 int i;
 //initializing the arrays
-char a[n];
-int b[n];
-long long int c[n];
+char arr1[n];
+int arr2[n];
+long long int arr3[n];
+
+//populating the array
+for ( i = 0; i < n; i++) {
+        arr1[i]=rand()%128;
+    }
 
 for ( i = 0; i < n; i++) {
-        a[i]=rand()%128;
-        b[i]=rand();
-        c[i]=rand()*rand();
+        arr2[i]=rand();
+    }
+
+for ( i = 0; i < n; i++) {
+        arr3[i]=rand()*rand();
     }
    
+    //printing the addresses and values
 printf("\nAddresses and values in charArray:\n");
+char *num;
     for ( i = 0; i < n; i++) {
-        printf("Address: %p, Value: %c\n", (void*)&a[i], a[i]);
+        printf("Address: %p, Value: %c\n", (arr1+i), *(arr1+i));
     }
    
     printf("\nAddresses and values in intArray:\n");
     for ( i = 0; i < n; i++) {
-        printf("Address: %p, Value: %d\n", (void*)&b[i], b[i]);
+        printf("Address: %p, Value: %d\n", arr2+i, *(arr2+i));
     }
 
     printf("\nAddresses and values in longLongIntArray:\n");
     for ( i = 0; i < n; i++) {
-        printf("Address: %p, Value: %lld\n", (void*)&c[i], c[i]);
+        printf("Address: %p, Value: %lld\n", arr3+i, *arr3+i);
     }
 }
